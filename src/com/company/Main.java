995738f5c77b7bf5e9b44488fcb77220a062a6ba
Mike Maleev <chip_01@bk.ru>
2020-05-04 00:45:46 +0300

    Sortirovka stroki HashMap 03.05

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,11 @@ public class Main {
         System.out.println("Эта программа сортирует строку по словам! \n      Введите строку из латинских символов");
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
+        Long start = System.currentTimeMillis();
         String sortedString = getSortString(s);
         System.out.println("Сортировка HashMap: \n" + sortedString);
+        Long end = System.currentTimeMillis();
+        System.out.println("Время работа программы: " + (end - start) + " ms");
     }
 
 
@@ -18,29 +21,31 @@ public class Main {
         //  StringBuilder builder = new StringBuilder();
         HashMap<String,Integer> map = new HashMap<>();
         for (int i = 0; i < words.length; i++) {
-                    int count = 0;
-                    String currentWord = words[i];
-                    for (String word : words) {
-                        if (currentWord.equals(word)) {
-                            count += 1;
-                        }
-                    }
-                    map.put(currentWord, count);
-                    //  builder.append( currentWord + " " + count + ";");
+            int count = 0;
+            String currentWord = words[i];
+            for (String word : words) {
+                if (currentWord.equals(word)) {
+                    count += 1;
+                }
+            }
+            map.put(currentWord, count);
+            //  builder.append( currentWord + " " + count + ";");
         }
-        List list = new ArrayList(map.entrySet());
+        List<Map.Entry<String,Integer>> list = new ArrayList(map.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<String,Integer>>() {
             @Override
-                    public int compare (Map.Entry<String,Integer> a, Map.Entry<String,Integer> b) {
+            public int compare (Map.Entry<String,Integer> a, Map.Entry<String,Integer> b) {
                 return a.getValue() - b.getValue();
             }
         });
         Collections.reverse(list);
 
-        /*map.entrySet().stream()
-                .sorted(Map.Entry.<String,Integer>comparingByValue());
-         System.out.println(map);
-        */
+//        List<Map.Entry<String,Integer>> sortedList = map
+//                .entrySet()
+//                .stream()
+//                .sorted(Map.Entry.<String,Integer>comparingByValue().reversed())
+//                .collect(Collectors.toList());
+//         System.out.println(sortedList);
 
         return String.valueOf(list);     //builder.toString();
     }
