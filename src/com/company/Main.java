8317174086c511624ff636af099cba4cd37e698a
Mike Maleev <chip_01@bk.ru>
2020-05-09 10:45:16 +0300

    Read text from file with dialog

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -8,22 +9,28 @@ import java.util.*;
 public class Main {
 
     private static int n = 20;
-    private static String pathname = "C:\\Users\\User\\Documents\\Java\\StringSort\\Text1.txt";
 
     public static void main(String[] args) throws IOException {
         System.out.println("Эта программа сортирует файл по словам! \n");
-        Long start = System.currentTimeMillis();
 
-        // Чтение файла и создание строки
-        Scanner in = new Scanner(new File(pathname));
-        StringBuilder builder = new StringBuilder();
-        while (in.hasNext())   builder.append(in.nextLine());
-        String s = builder.toString();
+        // Окно выбора файла
+        JFileChooser fileopen = new JFileChooser();
+        int ret = fileopen.showDialog(null, "Открыть файл");
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            File file = fileopen.getSelectedFile();
 
-        String sortedString = getSortString(s,n);
-        System.out.println(n + " наиболее часто встречающихся слов: \n" + sortedString);
-        Long end = System.currentTimeMillis();
-        System.out.println("\n Время работа программы: " + (end - start) + " ms");
+            // Чтение файла и создание строки
+            Long start = System.currentTimeMillis();
+            Scanner in = new Scanner(file);
+            StringBuilder builder = new StringBuilder();
+            while (in.hasNext()) builder.append(in.nextLine());
+            String s = builder.toString();
+
+            String sortedString = getSortString(s, n);
+            System.out.println(n + " наиболее часто встречающихся слов: \n" + sortedString);
+            Long end = System.currentTimeMillis();
+            System.out.println("\n Время работа программы: " + (end - start) + " ms");
+        }
     }
 
     public static String getSortString(String s, int n) {
